5945983af07f39cb463fffad6fa0d706f58d48bb
Kim Hajin <hk649@zoo.cs.yale.edu>
2024-09-17 11:36:18 -0400

    sha 512

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,6 +1,6 @@
 import csv
 from passlib.hash import md5_crypt
-from passlib.hash import sha256_crypt
+from passlib.hash import sha512_crypt
 
 # The hashed password string that was stolen
 hashed_password_string = "tbarron:$6$hZ.FAR/z/0NW4Uj6$7q9WOkYn7EmB9Xb0sPmdnLCWINwVrqnf9usF1/sUN7lpkNQOWewvmz.VxKmTAVcCdaP7ZdQjhY3.wJRo.vjkU.:19981:0:99999:7:::"
@@ -15,7 +15,7 @@ full_salt = f"${algorithm}${salt}$"
 
 def check_hash(input_string, hashed_password, full_salt):
     # Hash the input string with the full salt using passlib
-    hashed_value = sha256_crypt.using(salt=salt).hash(input_string)
+    hashed_value = sha512_crypt.using(salt=salt).hash(input_string)
     
     # Compare the hashed value with the hashed password
     return hashed_value.split('$')[-1] == hashed_password
